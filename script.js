c66d2ca9ff20b079eed94fb53d8e4789109b5a23
Hushbanks01 <chinedum5311@gmail.com>
2024-10-09 18:54:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+document.write("Hello World")
+
+//For subtraction//
+
+var girls = 5
+var boys = 10
+let students = boys - girls
+document.write("<br>")
+document.write(girls)
+function subtraction(girls, boys){
+    return girls - boys;
+}
+
+// For Addition //
+
+var car = 32
+var keke = 58
+motor = keke + car
+document.write("<br>")
+document.write(motor)
+function add(car, keke){
+    return car + keke;
+}
+
+//For Multiplication //
+
+let male = 10
+let female = 6
+let gender = male * female
+document.write("<br>")
+document.write(gender)
+function Multiplication(male, female){
+    return  male * female;
+}
+
+//For Division //
+
+const brothers = 20
+const sisters = 5
+const family = brothers / sisters
+document.write("<br>")
+document.write(family)
+function Division(brothers, sisters){
+    return brothers / sisters;
+}
